3de54dbdbb0a1cc5e69a2a4b41cf1661ee2f742b
shyamreghuonline <shyamreghu97@gmail.com>
2026-05-03 20:57:36 +0530

    merged

---
+++ src/types/firebase.d.ts
@@ -0,0 +1,11 @@
+declare module 'firebase/firestore' {
+  export * from 'firebase/firestore';
+}
+
+declare module 'firebase/auth' {
+  export * from 'firebase/auth';
+}
+
+declare module 'firebase/app' {
+  export * from 'firebase/app';
+}
